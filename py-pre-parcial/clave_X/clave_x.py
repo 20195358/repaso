ac3b8e09c630b4340246164141ce557f304475ba
20195358 <72094560+20195358@users.noreply.github.com>
2020-10-15 14:38:29 -0700

    Add files via upload

---
+++ py-pre-parcial/clave_X/clave_x.py
@@ -0,0 +1,62 @@
+import math
+
+"""
+***************************************************************
+@@ ejercicio 1 @@
+un metodo python que haga la suma de 2 numeros
+2+4 = 6
+"""
+
+
+# start-->
+def suma():
+    numero1 = 2
+    numero2 = 4
+    result = numero1 + numero2
+    return result
+
+
+"""
+***************************************************************
+@@ ejercicio 2 @@
+un metodo python que haga la multiplicacion de 3 numeros
+2*4*5 = 40
+"""
+
+
+# start-->
+def multiplicacion():
+    num1 = 2
+    num2 = 4
+    num3 = 5
+    result = num1 * num2* num3
+    return result
+
+
+"""
+***************************************************************
+@@ ejercicio 3 @@
+un metodo python que haga la suma de los numeros de la lista
+numerosLista = [2,5,4,6,9,12]
+"""
+
+
+# start-->
+def sumarLista():
+    thislist = [2,5,4,6,9,12]
+
+    return thislist[0] + thislist[1] + thislist[2] + thislist[3] + thislist[4] + thislist[5]
+
+
+"""
+***************************************************************
+@@ ejercicio 4 @@
+colocar este proyecto en github
+colocar aca debajo la url
+enviar la url al correo balbino_a@hotmail.com
+"""
+
+
+# github url-->
+def getGithubUrl():
+    return "https://github.com/20195358/repaso.git"
